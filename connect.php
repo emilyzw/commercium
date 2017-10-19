aeeba3bc510badb60edc11f76c29124b9286247d
JesseB123 <32357492+JesseB123@users.noreply.github.com>
2017-10-19 10:28:18 +0200

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,27 @@
+<?php
+
+DEFINE ('DB_USER', 'root');
+DEFINE ('DB_PASSWORD', '');
+DEFINE ('DB_HOST', 'localhost');
+DEFINE ('DB_NAME', 'survey'); 
+ 
+$dbc = @mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die ('could not connect to the database because: '. mysqli_connect_error );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
